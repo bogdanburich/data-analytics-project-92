c5038fdab9ce8504636687fc6ca8a2a55c40d88d
Bogdan Burich <bogdan.buritch@yandex.ru>
2023-04-06 21:43:28 +0300

    Try to pass top 10 total income

--- queries.sql
+++ queries.sql
@@ -7,7 +7,7 @@ left join sales s on e.employee_id = s.sales_person_id
 left join products p on p.product_id = s.product_id
 group by 1
 having count(s.sales_id) > 0
-order by 2 desc
+order by 3 desc
 
 -- Проверяем, что сумма продаж по всем продавцам совпадает с общей суммой продаж
 select sum(s.quantity * p.price)
